9a24f1c169361f6bc8c6a8923d07b890c67f226d
Magno Adauto <adauto.magno@totvs.com.br>
2025-04-02 11:59:48 -0300

    Api Java

---
+++ Rest Java/src/main/java/apiJavaPK/App.java
@@ -0,0 +1,21 @@
+package apiJavaPK;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class App extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain");
+        PrintWriter out = response.getWriter();
+        out.println("Hello World");
+    }
+}
